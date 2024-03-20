6b6af61d56b9bb33ffdb0b56bba1b9db06c7328d
unknown <prashant.champs@gmail.com>
2024-03-20 23:45:38 +0530

    added all application files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <h1>Welcome to 2024</h1>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
